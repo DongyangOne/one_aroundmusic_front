86d2174527a24ce7b40cc9483e29606ab522e045
zhco00 <85988984+zhco00@users.noreply.github.com>
2023-07-11 15:05:22 +0900

    obj
    obj파일 작동확인

--- App.js
+++ App.js
@@ -1,51 +1,121 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
-} from '@viro-community/react-viro';
+  ViroBox,
+  ViroMaterials,
+  ViroAnimations,
+  Viro3DObject,
+  ViroAmbientLight,
+} from "@viro-community/react-viro";
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
+const initialScene = props => {
+  let data = props.sceneNavigator.viroAppProps;
+  ViroMaterials.createMaterials({
+    wolf: {
+      diffuseTexture: require("./assets/obj/textures/Wolf_Body.jpg"),
+    },
+    skull: {
+      diffuseTexture: require("./assets/Skull/Skull.jpg"),
+    },
+  });
 
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    setText('Hello World!');
-  }
-
+  ViroAnimations.registerAnimations({
+    rotate: {
+      duration: 2500,
+      properties: {
+        rotateX: "+=90",
+      },
+    },
+    rotate1: {
+      duration: 2500,
+      properties: {
+        rotateY: "+=90",
+      },
+    },
+  });
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
+    <ViroARScene>
+      {/* <ViroText
+        text={"helloworld"}
+        position={[1, -1, -2]}
+        styles={{ fontSize: "30" }}
+  /> */}
+      {/* <ViroBox
+        height={1}
+        length={2}
+        width={1}
         scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+        position={[1, -1, -2]}
+        materials={["dog"]}
+        animation={{ name: "rotate", loop: true, run: true }}
+      /> */}
+      <ViroAmbientLight color="#ffffff" />
+      {data.object === "skull" ? (
+        <Viro3DObject
+          source={require("./assets/Skull/Skull.obj")}
+          position={[0, 0, -4]}
+          scale={[0.08, 0.08, 0.08]}
+          rotation={[-90, 0, 0]}
+          materials={"skull"}
+          animation={{ name: "rotate1", loop: true, run: true }}
+          type="OBJ"
+        />
+      ) : (
+        <Viro3DObject
+          source={require("./assets/obj/Wolf.obj")}
+          position={[0, -1, -2]}
+          scale={[2, 2, 2]}
+          rotation={[0, 50, 0]}
+          materials={"wolf"}
+          animation={{ name: "rotate1", loop: true, run: true }}
+          type="OBJ"
+        />
+      )}
     </ViroARScene>
   );
 };
-
 export default () => {
+  const [object, setObject] = useState("skull");
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
+    <View style={styles.mainView}>
+      <ViroARSceneNavigator
+        initialScene={{
+          scene: initialScene,
+        }}
+        viroAppProps={{ object: object }}
+        style={{ flex: 1 }}
+      />
+      <View style={styles.controlsView}>
+        <TouchableOpacity onPress={() => setObject("skull")}>
+          <Text style={styles.text}>display skull </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setObject("wolf")}>
+          <Text style={styles.text}>display wolf</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
+  mainView: { flex: 1 },
+  controlsView: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#ffffff",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  text: {
+    margin: 15,
+    backgroundColor: "#9d9d9d",
+    color: "black",
+    padding: 10,
+    fontWeight: "bold",
   },
 });
