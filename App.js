1674cb390aeae6d9f7b6c8e91018f0cd8c96b047
zhco00 <85988984+zhco00@users.noreply.github.com>
2023-07-24 16:00:25 +0900

    페이지 이동
    AR과 컴포넌트 페이지 이동

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
@@ -7,25 +8,109 @@ import {
   ViroConstants,
   ViroARSceneNavigator,
 } from '@viro-community/react-viro';
+=======
+// import React, { useState } from "react";
+// import { StyleSheet } from "react-native";
+// import {
+//   ViroARScene,
+//   ViroText,
+//   ViroConstants,
+//   ViroARSceneNavigator,
+// } from "@viro-community/react-viro";
+>>>>>>> Stashed changes
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
+// import { View, Text, Image, ScrollView, TextInput, Button } from "react-native";
+// import HomeScreen from "./View/screens/HomeScreen";
+// const HelloWorldSceneAR = () => {
+//   const [text, setText] = useState("Initializing AR...");
 
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    setText('Hello World!');
-  }
+//   function onInitialized(state, reason) {
+//     console.log("guncelleme", state, reason);
+//     setText("Hello World!");
+//   }
 
+//   return (
+//     <ViroARScene onTrackingUpdated={onInitialized}>
+//       <ViroText
+//         text={text}
+//         scale={[0.5, 0.5, 0.5]}
+//         position={[0, 0, -1]}
+//         onClick={HelloSceneAR}
+//         style={styles.helloWorldTextStyle}
+//       />
+//     </ViroARScene>
+//   );
+// };
+// // const HelloSceneAR = () => {
+// //   const [text, setText] = useState("Initializing AR...");
+
+// //   function onInitialized(state, reason) {
+// //     console.log("guncelleme", state, reason);
+// //     setText("World!");
+// //   }
+
+// //   return (
+// //     <ViroARScene onTrackingUpdated={onInitialized}>
+// //       <ViroText
+// //         text={text}
+// //         scale={[0.5, 0.5, 0.5]}
+// //         position={[0, 0, -1]}
+// //         onClick={HomeScreen}
+// //         style={styles.helloWorldTextStyle}
+// //       />
+// //     </ViroARScene>
+// //   );
+// // };
+
+// const App = () => {
+//   return (
+//     <ScrollView>
+//       <Text>Some text</Text>
+//       <View>
+//         <Text>Some more text</Text>
+//         <Image
+//           source={{
+//             uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+//           }}
+//           style={{ width: 200, height: 200 }}
+//         />
+//       </View>
+
+//       {/* <Button title="hi" onPress={}/> */}
+//       <Button title="hi" onPress={HomeScreen} />
+//     </ScrollView>
+//   );
+// };
+
+// var styles = StyleSheet.create({
+//   f1: { flex: 1 },
+//   helloWorldTextStyle: {
+//     fontFamily: "Arial",
+//     fontSize: 30,
+//     color: "#ffffff",
+//     textAlignVertical: "center",
+//     textAlign: "center",
+//   },
+// });
+// export default App;
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+/* routing screen */
+import Router_1 from "./Screen/Router_1";
+import Router_2 from "./Screen/Router_2";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="one">
+        <Stack.Screen name="one" component={Router_1} />
+        <Stack.Screen name="two" component={Router_2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+<<<<<<< Updated upstream
 };
 
 export default () => {
@@ -151,5 +236,8 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
+}
+>>>>>>> Stashed changes
+=======
 }
 >>>>>>> Stashed changes
--- Screen/Router_1.js
+++ Screen/Router_1.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import { Button } from "react-native";
 
@@ -10,4 +11,28 @@ const Router_1 = () => {
   );
 };
 
+=======
+import React, { useState } from "react";
+import { Button, View, Text } from "react-native";
+
+function Router_1({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to AR"
+        onPress={() => navigation.navigate("two")} // Details로 화면 이동
+      />
+    </View>
+  );
+
+  // <ViroARSceneNavigator
+  //   autofocus={true}
+  //   initialScene={{
+  //     scene: HelloWorldSceneAR,
+  //   }}
+  //   style={styles.f2}
+  // />
+}
+>>>>>>> Stashed changes
 export default Router_1;
