ac7c7b6c85a0752355d8af69a864cd5932240b2e
marie-lee <mariesejinlee@gmail.com>
2023-08-06 03:29:02 +0900

    메인페이지 스토리 리스트

--- views/screens/Main.js
+++ views/screens/Main.js
@@ -5,24 +5,57 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
+  FlatList,
+  View,
 } from "react-native";
 
 import Header from "../components/Header";
+import MainStory from "../components/MainStory";
+
+const DATA = [
+  {
+    id: 'minkue',
+    src: require("../../assets/tiger.jpeg"),
+  },
+  {
+    id: 'sejin',
+    src: require("../../assets/tiger.jpeg"),
+  },
+  {
+    id: 'minkue',
+    src: require("../../assets/tiger.jpeg"),
+  },
+  {
+    id: 'minkue',
+    src: require("../../assets/tiger.jpeg"),
+  },
+];
+
+const FriendItem = ({id, src}) => (
+  <View>
+    <MainStory id={id} src={src} />
+  </View>
+);
 
 const Main = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Header/>
+          <Header />
+          {/* <MainStory /> */}
+          <FlatList
+          data={DATA}
+          renderItem={({item}) => (
+            <FriendItem id={item.id} src={item.src}/>
+          )}
+          numColumns={5}/>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-  },
-
+  container: {},
 });
 
 export default Main;
