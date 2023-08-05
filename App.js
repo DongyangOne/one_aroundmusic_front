61a98cf008fa73423ae75642755f6db1bb786d3d
zhco00 <85988984+zhco00@users.noreply.github.com>
2023-08-05 23:57:00 +0900

    scroll and swipe

--- App.js
+++ App.js
@@ -1,51 +1,46 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
-import {
-  ViroARScene,
-  ViroText,
-  ViroConstants,
-  ViroARSceneNavigator,
-} from '@viro-community/react-viro';
-
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
-
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    setText('Hello World!');
-  }
-
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
-  );
-};
-
-export default () => {
-  return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
-  );
-};
-
-var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
+import React from "react";
+import { Text, View } from "react-native";
+import Swiper from "react-native-swiper";
+import Main from "./screens/Main";
+import Map from "./screens/Map";
+import Music from "./screens/Music";
+var styles = {
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
   },
-});
+  slide2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#97CAE5",
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#92BBD9",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+};
+
+export default () => (
+  <Swiper style={styles.wrapper} loop={false}>
+    <View testID="Hello" style={styles.slide1} component>
+      <Main />
+    </View>
+    <View testID="Beautiful" style={styles.slide2}>
+      <Map />
+    </View>
+    <View testID="Simple" style={styles.slide3}>
+      <Music />
+    </View>
+  </Swiper>
+);
