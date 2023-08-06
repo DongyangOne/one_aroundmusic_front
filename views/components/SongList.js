109741b988b324598ed63ad17643b4080f1271f4
최혜림 <chhari0708@naver.com>
2023-08-06 19:13:00 +0900

    추천페이지

---
+++ views/components/SongList.js
@@ -0,0 +1,54 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+const SongList = ({title, image, singer, date}) => {
+  return (
+    <View style={styles.wrap}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={image} />
+        <View style={styles.text}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.singer}>{singer}</Text>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+      </View>
+      <View style={styles.line} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrap: {},
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 7,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  text: {
+    marginLeft: 17,
+    marginTop: 17,
+  },
+  title: {
+    fontSize: 17,
+    color: '#034AA6',
+    fontWeight: 'bold',
+  },
+  singer: {
+    fontSize: 12,
+    color: '#D0D0D0',
+  },
+  date: {
+    fontSize: 10,
+    color: '#D0D0D0',
+  },
+  line: {
+    height: 1,
+    width: 370,
+    backgroundColor: '#034AA6',
+    marginVertical: 15,
+  },
+});
+
+export default SongList;
--- views/screens/Music.js
+++ views/screens/Music.js
@@ -1,22 +1,170 @@
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
   Text,
   SafeAreaView,
   ScrollView,
+  StatusBar,
+  View,
   Button,
-} from "react-native";
+  FlatList,
+  Image,
+} from 'react-native';
 
-import Header from "../components/Header";
+import Header from '../components/Header';
+import FilterButton from '../components/FilterButton';
+import SongList from '../components/SongList';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const Msuic = ({ navigation }) => {
+const FILTER = [
+  {
+    id: 1,
+    title: '나이',
+  },
+  {
+    id: 2,
+    title: '성별',
+  },
+  {
+    id: 3,
+    title: '날씨',
+  },
+  {
+    id: 4,
+    title: '시간',
+  },
+  {
+    id: 5,
+    title: '계절',
+  },
+];
+
+const DATA = [
+  {
+    id: 1,
+    tag: '#10대',
+  },
+  {
+    id: 2,
+    tag: '#봄',
+  },
+  {
+    id: 1,
+    tag: '#여성',
+  },
+];
+
+const SONG = [
+  {
+    id: 1,
+    title: '봄봄봄',
+    singer: '로이킴',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album1.png'),
+  },
+  {
+    id: 2,
+    title: '봄이좋냐',
+    singer: '10cm',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album2.png'),
+  },
+  {
+    id: 3,
+    title: '벚꽃엔딩',
+    singer: '버스커 버스커',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album3.png'),
+  },
+  {
+    id: 1,
+    title: '봄봄봄',
+    singer: '로이킴',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album1.png'),
+  },
+  {
+    id: 2,
+    title: '봄이좋냐',
+    singer: '10cm',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album2.png'),
+  },
+  {
+    id: 3,
+    title: '벚꽃엔딩',
+    singer: '버스커 버스커',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album3.png'),
+  },
+  {
+    id: 1,
+    title: '봄봄봄',
+    singer: '로이킴',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album1.png'),
+  },
+  {
+    id: 2,
+    title: '봄이좋냐',
+    singer: '10cm',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album2.png'),
+  },
+  {
+    id: 3,
+    title: '벚꽃엔딩',
+    singer: '버스커 버스커',
+    date: '2013 . 04 . 22',
+    image: require('../../assets/album/album3.png'),
+  },
+];
+
+const Music = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <View style={{flexDirection: 'row'}}>
+        <FlatList
+          style={styles.filter}
+          data={FILTER}
+          renderItem={({item}) => (
+            <FilterButton
+              color="#EBEBEB"
+              textColor="#656565"
+              title={item.title}
+              onPress={() => navigation.navigate('Filter')}
+            />
+          )}
+          numColumns={5}
+        />
+        <TouchableOpacity onPress={() => navigation.navigate('Filter')}>
+          <Image
+            style={{marginTop: 34, width: 20, height: 20, marginRight: 20}}
+            source={require('../../assets/filterIcon.png')}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        style={styles.selected}
+        data={DATA}
+        renderItem={({item}) => (
+          <FilterButton color="#034AA6" textColor="#ffffff" title={item.tag} />
+        )}
+        numColumns={3}
+      />
       <ScrollView style={styles.scrollView}>
-        <Button
-          title="Press me"
-          onPress={() => navigation.navigate("Filter")}
+        <FlatList
+          data={SONG}
+          renderItem={({item}) => (
+            <SongList
+              image={item.image}
+              title={item.title}
+              singer={item.singer}
+              date={item.date}
+            />
+          )}
         />
       </ScrollView>
     </SafeAreaView>
@@ -26,12 +174,25 @@ const Msuic = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {},
   scrollView: {
-    backgroundColor: "pink",
-    marginHorizontal: 20,
+    marginTop: 27,
+    width: 400,
+    height: 650,
+    marginLeft: 16,
   },
   text: {
     fontSize: 42,
   },
+  filter: {
+    flexDirection: 'row',
+    marginTop: 30,
+    marginLeft: 16,
+    height: 30,
+  },
+  selected: {
+    height: 30,
+    marginTop: 16,
+    marginLeft: 16,
+  },
 });
 
-export default Msuic;
+export default Music;
