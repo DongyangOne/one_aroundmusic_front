315d5b820082e476561fd194f9986aa5bfd2feb9
marie-lee <mariesejinlee@gmail.com>
2023-08-06 19:12:58 +0900

    음악 스트리밍

---
+++ views/screens/Listening.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import Header from "../components/Header";
+
+const Listening = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.back}>
+        <ImageBackground
+          source={require("../../assets/album.png")}
+          style={styles.album1}
+          blurRadius={10}
+        >
+          <ImageBackground style={styles.back}>
+            <Image
+              source={require("../../assets/album2.png")}
+              style={styles.album2}
+            />
+            <Text style={styles.title}>봄봄봄</Text>
+            <Text style={styles.singer}>로이킴</Text>
+            <Image source={require("../../assets/streaming.png")} style={styles.stream}/>
+            <Image source={require("../../assets/play.png")} style={styles.play}/>
+            <TouchableOpacity onPress={() => navigation.navigate("ArScreen")}>
+                <Image source={require("../../assets/share.png")} style={styles.share}/>
+            </TouchableOpacity>
+          </ImageBackground>
+        </ImageBackground>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center"
+  },
+  back: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    alignItems: "center"
+  },
+  album1: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    width: 400,
+    height: 800,
+    resizeMode: "cover",
+  },
+  album2: {
+    width: 206,
+    height: 206,
+    marginTop: 114,
+  },
+  title:{
+    fontSize: 24,
+    fontStyle: "bold",
+    color: "white",
+    marginTop: 31,
+  },
+  singer:{
+    fontSize: 13,
+    fontWeight: "light",
+    color: "white",
+    marginTop: 12,
+  },
+  stream:{
+    width: 350,
+    height:13,
+    marginTop: 51,
+  },
+  play:{
+    width: 180, 
+    height: 36,
+    marginTop: 30,
+  },
+  share:{
+    width: 251,
+    height: 35,
+    borderRadius: 5,
+    marginTop: 50,
+  }
+});
+
+export default Listening;
--- views/screens/Music.js
+++ views/screens/Music.js
@@ -8,22 +8,13 @@ import {
 } from "react-native";
 
 import Header from "../components/Header";
+import Listening from "./Listening";
 
-const Msuic = () => {
+const Music = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Header/>
       <ScrollView style={styles.scrollView}>
-        
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
+        <Listening/>
       </ScrollView>
     </SafeAreaView>
   );
@@ -31,13 +22,10 @@ const Msuic = () => {
 
 const styles = StyleSheet.create({
   container: {},
-  scrollView: {
-    backgroundColor: "pink",
-    marginHorizontal: 20,
-  },
+
   text: {
     fontSize: 42,
   },
 });
 
-export default Msuic;
+export default Music;
