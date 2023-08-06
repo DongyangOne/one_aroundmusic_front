8e4f71153c2a8b592049c0c0d4b04a2d1b3fb16a
rlawnaks <rlawnaks1234@naver.com>
2023-08-06 18:29:27 +0900

    처음 지도 페이지 이동시 현재 위치 표시 및 중심으로

--- views/screens/Map.js
+++ views/screens/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,31 +7,70 @@ import {
   TouchableOpacity,
   Image,
   View,
+  Platform,
+  PermissionsAndroid,
 } from "react-native";
 import MapView from 'react-native-maps';
-import Header from "../components/Header";
+import Geolocation from "react-native-geolocation-service";
 
+async function requestPermission() {
+  try {
+    if (Platform.OS === "ios") {
+      return await Geolocation.requestAuthorization("always");
+    }
+    // 안드로이드 위치 정보 수집 권한 요청
+    if (Platform.OS === "android") {
+      return await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 const Map = ({ navigation }) => {
+  const [location, setLocation] = useState();
+  useEffect(() => {
+    requestPermission().then(result => {
+      console.log({ result });
+      if (result === "granted") {
+        Geolocation.getCurrentPosition(
+          pos => {
+            setLocation(pos.coords);
+          },
+          error => {
+            console.log(error);
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 3600,
+            maximumAge: 3600,
+          },
+        );
+      }
+    });
+  }, []);
+
+  if (!location) {
+    return (
+      <View>
+        <Text>Splash Screen</Text>
+      </View>
+    );
+  }
+
   return (
-    //<SafeAreaView style={styles.container}>
-      //<ScrollView>
-        //<TouchableOpacity onPress={() => navigation.navigate("ArScreen")}>
-          //<Header />
-          <View style={{ flex: 1 }}>
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 37.566535,
-          longitude: 126.9779692,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.05,
         }}
+        showsUserLocation={true}
       />
-    </View>
-        //</TouchableOpacity>
-      //</ScrollView>
-    //</SafeAreaView>
   );
 };
 
