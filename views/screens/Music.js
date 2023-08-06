bc0091cd9db34c2ff04818f0243cd3fe2f692826
zhco00 <85988984+zhco00@users.noreply.github.com>
2023-08-06 18:59:48 +0900

    error수정

--- views/screens/Music.js
+++ views/screens/Music.js
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  Button,
-} from "react-native";
-
+import { StyleSheet, Text, SafeAreaView, Button } from "react-native";
+import { ScrollView } from "react-native-virtualized-view";
 import Header from "../components/Header";
 
 const Msuic = ({ navigation }) => {
