87f55520ef04cfc29870cf819601c07bcabbce70
FarmBera <ngyeo21@gmail.com>
2023-09-23 18:10:39 +0900

    Add ListenKing Profile Outlines

--- views/components/MainStory.js
+++ views/components/MainStory.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import {View, StyleSheet, Image, ScrollView} from 'react-native';
-import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import {
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { IMG_SRC } from '../screens/ListenKing';
 
-const MainStory = ({data}) => {
-  return (
-    <ScrollView
-      style={styles.scroll}
-      nestedScrollEnabled={true}
-      horizontal={true}
-    >
-      <View style={styles.storyRow}>
-        {data.map(item => (
-          <View style={styles.story}>
-            <TouchableWithoutFeedback>
-              <Image source={item.src} style={styles.image}></Image>
-            </TouchableWithoutFeedback>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-  );
+const MainStory = ({ data, frame }) => {
+  try {
+    // console.log(`frame: ${frame.setData}`); // console loooooooooooooog
+    return (
+      <ScrollView
+        style={styles.scroll}
+        nestedScrollEnabled={true}
+        horizontal={true}>
+        <View style={styles.storyRow}>
+          {/* New Code */}
+          {data.map((item, index) => (
+            <View style={styles.story}>
+              <TouchableWithoutFeedback>
+                {index == 0 ? (
+                  <ImageBackground
+                    source={IMG_SRC[frame.setData].src}
+                    style={styles.imageBg}>
+                    <Image source={data[0].src} style={styles.imageSe}></Image>
+                  </ImageBackground>
+                ) : (
+                  <Image source={item.src} style={styles.image}></Image>
+                )}
+              </TouchableWithoutFeedback>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  } catch (error) {
+    // console.log(`NO DATA IMPORTED`); // Console looooooooooooooooooooog
+    return (
+      <ScrollView
+        style={styles.scroll}
+        nestedScrollEnabled={true}
+        horizontal={true}>
+        <View style={styles.storyRow}>
+          {/* Previous Code */}
+          {data.map(item => (
+            <View style={styles.story}>
+              <TouchableWithoutFeedback>
+                <Image source={item.src} style={styles.image}></Image>
+              </TouchableWithoutFeedback>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -36,6 +73,30 @@ const styles = StyleSheet.create({
     borderRadius: 28,
     marginLeft: 18,
     marginTop: 18,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 4,
+  },
+  imageBg: {
+    width: 78,
+    height: 78,
+    borderRadius: 28,
+    marginLeft: 5,
+    marginTop: 5,
+    // borderWidth: 1,
+    // borderColor: "white",
+    // backgroundColor: "#0ff", // Just for Test
+    // padding: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageSe: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    // marginLeft: 18,
+    // marginTop: 18,
+    marginLeft: 1,
     borderWidth: 1,
     borderColor: 'white',
     padding: 4,
--- views/screens/ListenKing.js
+++ views/screens/ListenKing.js
@@ -1,78 +1,138 @@
-import React, { useState } from "react";
-import { Image, StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import React, { useState, useNavigate } from 'react';
+import { Image, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+import Main from './Main';
 
 const RoundedShadowBox = ({ children }) => {
-  return (
-    <View style={styles.roundedShadowBox}>
-      {children}
-    </View>
-  );
+  return <View style={styles.roundedShadowBox}>{children}</View>;
 };
+
+export const IMG_SRC = [
+  {
+    // Black Circle Outline
+    id: 'reward_black',
+    src: require('../../assets/listen1.png'),
+  },
+  {
+    // Rainbow Circle (Default)
+    id: 'reward_rainbow',
+    src: require('../../assets/listen2.png'),
+  },
+  {
+    // Purple Circle
+    id: 'reward_purple',
+    src: require('../../assets/listen3.png'),
+  },
+];
 
 const ListenKing = ({ navigation }) => {
   const [images, setImages] = useState([
-    require("../../assets/listen1.png"),
-    require("../../assets/listen2.png"),
-    require("../../assets/listen3.png"),
+    require('../../assets/listen1.png'), // Black Circle Outline
+    require('../../assets/listen2.png'), // Rainbow Circle (Default)
+    require('../../assets/listen3.png'), // Purple Circle
   ]);
   const [abc, setAbc] = useState([
-    require("../../assets/listenReward1.png"),
-    require("../../assets/listenReward2.png"),
-    require("../../assets/listenReward3.png"),
+    require('../../assets/listenReward1.png'),
+    require('../../assets/listenReward2.png'),
+    require('../../assets/listenReward3.png'),
   ]);
-  
+
+  /** Currently selected ListenKing */
+  const [selection, setSelection] = useState(1);
+
+  const NOW_SET = dir => {
+    // console.log(selection);
+    let temp = selection;
+    /** 'dir' means Direction
+     * An index determining the direction of increase/decrease
+     * (Which way to increse/decrease index)
+     * r: Right
+     * l: Left
+     */
+    if (dir == 'r') {
+      temp++;
+      if (temp > 2) temp = 0;
+    } else if (dir == 'l') {
+      temp--;
+      if (temp < 0) temp = 2;
+    } else {
+      console.log(`dir: ${dir}. ERROR Occurred!`);
+      return;
+    }
+    setSelection(temp);
+  };
+
   const imageStyles = [
     {
       width: 80,
       height: 80,
-      marginTop: "50%",
+      marginTop: '50%',
       marginRight: 35,
     }, // 스타일1
     {
       width: 150,
       height: 150,
-      marginTop: "15%",
-      alignItems: "center",
+      marginTop: '15%',
+      alignItems: 'center',
     }, // 스타일2
-    { width: 80, height: 80, marginTop: "50%", marginLeft: 35 }, // 스타일3
+    { width: 80, height: 80, marginTop: '50%', marginLeft: 35 }, // 스타일3
   ];
 
   const handleLeftImageClick = () => {
     setImages(prevImages => [prevImages.pop(), ...prevImages]);
     setAbc(prevImages => [prevImages.pop(), ...prevImages]);
+    NOW_SET('l');
   };
 
   const handleRightImageClick = () => {
     setImages(prevImages => [...prevImages.slice(1), prevImages[0]]);
     setAbc(prevImages => [...prevImages.slice(1), prevImages[0]]);
+    NOW_SET('r');
   };
 
   const handleGoBack = () => {
     navigation.goBack();
   };
 
+  /** Go to the main screen */
+  const handleGoHome = () => {
+    navigation.navigate('Main', {
+      setData: selection,
+    });
+  };
+
   return (
     <>
-        <View style={styles.centeredContainer}>
-          <Image style={styles.LogoMain} source={abc[1]} />
-        </View>
-
       <View style={styles.centeredContainer}>
+        <Image style={styles.LogoMain} source={abc[1]} />
       </View>
+
+      <View style={styles.centeredContainer}></View>
       <RoundedShadowBox>
-      <TouchableOpacity style={styles.serve}>
-        <TouchableOpacity onPress={handleLeftImageClick}>
-          <Image style={imageStyles[0]} source={images[0]} />
+        <TouchableOpacity style={styles.serve}>
+          <TouchableOpacity
+            onPress={() => {
+              handleLeftImageClick();
+            }}>
+            <Image style={imageStyles[0]} source={images[0]} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={imageStyles[1]} source={images[1]} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              handleRightImageClick();
+            }}>
+            <Image style={imageStyles[2]} source={images[2]} />
+          </TouchableOpacity>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Image style={imageStyles[1]} source={images[1]} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleRightImageClick}>
-          <Image style={imageStyles[2]} source={images[2]} />
-        </TouchableOpacity>
-      </TouchableOpacity>
       </RoundedShadowBox>
-      <TouchableOpacity style={styles.button} onPress={handleGoBack}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          // handleGoBack();
+          handleGoHome(selection); // Move to main screen
+        }}>
         <Text style={styles.buttonText}>적용하기</Text>
       </TouchableOpacity>
     </>
@@ -81,48 +141,48 @@ const ListenKing = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    marginTop: "30%",
+    alignItems: 'center',
+    marginTop: '30%',
   },
   LogoMain: {
     marginTop: 100,
     marginBottom: 50,
     width: 150,
-    height: 150, 
-    alignSelf: "center", 
+    height: 150,
+    alignSelf: 'center',
   },
   serve: {
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   button: {
-    backgroundColor: "#000080",
+    backgroundColor: '#000080',
     paddingVertical: 8,
     paddingHorizontal: 30,
     borderRadius: 5,
     marginHorizontal: 30,
-    alignItems: "center",
-    marginTop: "30%",
+    alignItems: 'center',
+    marginTop: '30%',
   },
   buttonText: {
-    color: "white",
+    color: 'white',
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   centeredContainer: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   centeredImage: {
-    width: 60, 
+    width: 60,
     height: 60,
   },
   roundedShadowBox: {
     width: 320,
     marginLeft: 50,
     marginTop: 30,
-    overflow: "hidden",
+    overflow: 'hidden',
     borderRadius: 10,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     elevation: 5,
     padding: 10,
     margin: 10,
