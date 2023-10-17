789f7a0ae8924f4b656016fdf093f1d8d510aa36
mhyoc <105059385+mhyoc@users.noreply.github.com>
2023-10-18 01:00:43 +0900

    update pop

--- src/screens/ArScreen.js
+++ src/screens/ArScreen.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Image,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import {
   ViroARScene,
   ViroImage,
   ViroARSceneNavigator,
 } from '@viro-community/react-viro';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import storage from '@react-native-firebase/storage';
 
-const WorldSceneAR = () => {
+const serverURL = 'http://125.133.34.224:8001'; // DB Server URL
+let loadData = null; // DB에서 불러온 데이터 저장
+export let TOKEN = null;
+let temp;
+
+const WorldSceneAR = ({ data, frame }) => {
+  const [itemFrame, setItemFrame] = useState();
+  const [selectId, setSelectId] = useState();
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('accessToken');
+      if (value !== null) {
+        TOKEN = value;
+        axios
+          .get(`${serverURL}/api/reward/pop`, {
+            headers: {
+              Authorization: `Bearer ${TOKEN}`,
+            },
+          })
+          .then(response => {
+            loadData = response.data;
+            /** Attempt 3 */
+            // console.log(loadData);
+            temp = loadData.data.selectedReward.id;
+            setSelectId(temp);
+            setData();
+          })
+          .catch(e => {
+            console.error(`GET ERROR >> ${e}`);
+          });
+      } else {
+        console.log('No data found');
+      }
+    } catch (e) {
+      console.error(`Error with Reading Data >> ${e}`);
+    }
+  };
+
+  /** Attempt 5 */
+  const setData = async () => {
+    text = `/reward/pop/border${temp - 6}.png`;
+    // console.log(`text >> ${text}`);
+    setItemFrame(await storage().ref(text).getDownloadURL());
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <ViroARScene>
       <ViroImage
