3dbdd1d76e1c4aecd8a4e6fb797cd8d29abbc03c
최혜림 <chhari0708@naver.com>
2023-10-19 23:15:50 +0900

    아이디 비번 예외처리

--- src/screens/StartingPage.js
+++ src/screens/StartingPage.js
@@ -83,6 +83,7 @@ import {
   View,
   Text,
   Pressable,
+  ToastAndroid,
 } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -119,6 +120,7 @@ function StartingPage({ navigation }) {
       await AsyncStorage.setItem('accessToken', response.data.data.access);
       navigation.replace('Main');
     } catch (error) {
+      ToastAndroid.show('비밀번호와 아이디를 확인하세요', ToastAndroid.LONG);
       console.error('Error:', error);
     }
   };
